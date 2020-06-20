c6f9ec3b66fa0e6308962c48c5c639f49d89ad62
Joeseph Makram <jojomak350@gmail.com>
2020-06-20 17:33:26 +0000

    finish the entire task

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 var cors = require('cors');
 
@@ -15,6 +16,7 @@ var port = process.env.PORT || 3000;
 // mongoose.connect(process.env.DB_URI);
 
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
@@ -25,12 +27,12 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.use('/', require('./myApp'));
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
